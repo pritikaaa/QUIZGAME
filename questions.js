a0a2af2205fd378320c3a870be11316132dcf6dc
priti <pritiranabhat2@gmail.com>
2026-08-17 18:40:56 +0545

    added retouches

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions =[
+    {
+        numb: 1,
+        question:"Number of Bytes reserved for 1 integer, 1 character and 1 float variable is...",
+        answer:"D. 7",
+        options: [
+            "A. 3",
+            "B. 4",
+            "C. 6",
+            "D. 7"
+        ]
+    },
+
+{
+    numb: 2,
+    question:"In which normal form of database, atomicity is introduced?",
+    answer:"C. Third",
+    options: [
+            "A. First",
+            "B. Second",
+            "C. Third",
+            "D. Fourth"
+    ]
+},
+
+
+{
+    numb: 3,
+    question:"Which of the following keywords are used to declare a variable in JavaScript?",
+    answer:"C. var or let",
+    options: [
+            "A. int or var",
+            "B. float or let",
+            "C. var or let",
+            "D. char or var"
+    ]
+},
+
+{
+    numb: 4,
+    question:"DFD stands for......",
+    answer:"A. Data Flow Diagram",
+    options: [
+            "A. Data Flow Diagram",
+            "B. Data File Diagram",
+            "C. Data Fixed Diagram",
+            "D. None of the above"
+    ]
+},
+
+{
+    numb: 5,
+    question: "What is called the user has identified via a username and password in DBMS?", 
+    answer:"B. Authorization",
+    options: [
+            "A. Authentication",
+            "B. Authorization",
+            "C. Accounting",
+            "D. Auditing"
+    ]
+}
+];
